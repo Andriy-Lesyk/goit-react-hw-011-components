7a082c4af3ae4dad72e75459dc3f158139c67b70
Andriy-Lesyk <74345435+Andriy-Lesyk@users.noreply.github.com>
2022-03-30 18:28:01 +0300

    Робить перше завдання

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import {Container, Description, Photo, Name, Location, Tag} from "./Profile.styles"
+import {Container, Description, Photo, Name, Location, Tag, StatsItems, StItem} from "./Profile.styles"
 
 const Profile=({ avatar, tag, name, location, statis})=> {
   return (
       <Container>
             <Description>
-              <Photo src={avatar}/>
-              <Name>{name}</Name>
-              <Tag>{tag}</Tag>
-              <Location>{location}</Location>
+                <Photo src={avatar}/>
+                <Name>{name}</Name>
+                <Tag>{tag}</Tag>
+                <Location>{location}</Location>
             </Description>
+            <StatsItems>
+                <StItem>
+                    <span class="label">Followers</span>
+                    <span class="quantity">{statis.followers}</span>
+                </StItem>
+                <StItem>
+                    <span class="label">Views</span>
+                    <span class="quantity">{statis.views}</span>
+                </StItem>
+                <StItem>
+                    <span class="label">Likes</span>
+                    <span class="quantity">{statis.likes}</span>
+                </StItem>
+            </StatsItems>
           
         </Container>
   )
--- src/components/Profile/Profile.styles.jsx
+++ src/components/Profile/Profile.styles.jsx
@@ -1,13 +1,63 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div``
+export const Container = styled.div`
+margin:40px;
+background-color: #E7E3E3;
+padding:60px;
+display:flex;
+flex-direction: column;
+align-items:center;
+`
 
-export const Description = styled.div``
+export const Description = styled.div`
+background-color:white;
+display: flex;
+flex-direction: column;
+justify-content:center;
+`
     
-export const Photo = styled.img``
+export const Photo = styled.img`
+border-radius: 50%;
+width: 100px;
+padding:30px 60px 15px 60px ;
+
+`
     
-export const Name = styled.p``
+export const Name = styled.span`
+align-items:center;
+display: flex;
+justify-content:center;
+font-size: 20px;
+font-weight:600;
+padding-bottom:15px;
+`
 
-export const Tag = styled.p``
+export const Tag = styled.span`
+display: flex;
+justify-content:center;
+padding-bottom:15px;
+`
 
-export const Location = styled.p``
+export const Location = styled.span`
+display: flex;
+justify-content:center;
+padding-bottom:25px;
+`
+
+export const StatsItems = styled.ul`
+background-color:white;
+display: flex;
+padding:0;
+margin:0;
+width: 220px;
+background-color:#e0e0e0;
+`
+    
+export const StItem = styled.li`
+display:flex;
+flex-direction:column;
+align-items:center;
+border: solid 1px #b8b2b2;
+width: 220px;
+padding: 10px 10px;
+`
